8ffb3394db546cd75d8dd874b80c8cd50f41dfc5
Riccardo <riccardolipparini0@gmail.com>
2021-12-20 15:13:12 +0100

    es finito

--- index.php
+++ index.php
@@ -5,8 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+
+        $p="ciao ciao ciao wow lorem ipus ok bla bla bla blallalalal bala bla bla";
+        
+        $word = $_GET["name"];
+
+        $censored = str_replace($word, "***", $p);
+    ?>
 </head>
 <body>
-    <h1>hello world!!</h1>
+    <p>
+
+        <p><?php echo '>la frase censorizzata è: ' . $censored; ?></p>
+
+        <?php
+
+            echo $p . '<br>';
+            echo $word;
+        
+        ?>
+    </p>
+    <p>
+        <?php
+
+            echo strlen($p);
+        
+        ?>
+    </p>
+    
 </body>
 </html>
